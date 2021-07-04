ebe5b37272dbef24598f5265eba29933b02d5a55
jkcoxson@gmail.com <jkcoxson@gmail.com>
2021-07-04 16:13:56 -0600

    Add manifest to plugin

--- plugClass.js
+++ plugClass.js
@@ -5,20 +5,12 @@ const winston = require('winston');
 const camelLibjs = require('./camelLib');
 
 module.exports = class plugin {
-<<<<<<< HEAD
     constructor(mappedClass, commands, manifest){
         this.class=mappedClass;
         this.commands=commands
         this.name=manifest.name
         this.description=manifest.description
         this.manifest=manifest
-=======
-    constructor(mappedClass, commands, name, description){
-        this.class=mappedClass;
-        this.commands=commands
-        this.name=name
-        this.description=description
->>>>>>> b48662d013d1ad3ec0bf0c83ff855c8c69463047
     }
     /**@type {Class} */
     class;
@@ -28,9 +20,6 @@ module.exports = class plugin {
     name;
     /**@type {String} */
     description;
-<<<<<<< HEAD
     /**@type {Object} */
-    manifest
-=======
->>>>>>> b48662d013d1ad3ec0bf0c83ff855c8c69463047
+    manifest;
 }
