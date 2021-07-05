1651a6a14bc71d8c81234839e0401f2fa9692abc
sb64 <53383020+sb64@users.noreply.github.com>
2021-07-05 00:06:50 -0600

    change parser

--- plugClass.js
+++ plugClass.js
@@ -5,12 +5,12 @@ const winston = require('winston');
 const camelLibjs = require('./camelLib');
 
 module.exports = class plugin {
-    constructor(mappedClass, commands, manifest){
-        this.class=mappedClass;
-        this.commands=commands
-        this.name=manifest.name
-        this.description=manifest.description
-        this.manifest=manifest
+    constructor(mappedClass, commands, manifest) {
+        this.class = mappedClass;
+        this.commands = commands
+        this.name = manifest.name
+        this.description = manifest.description
+        this.manifest = manifest
     }
     /**@type {Class} */
     class;
